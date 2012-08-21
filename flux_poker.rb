65d0cdb7be581ba2b04ef5a752d782b17cf333a4
Ben-Alexander Cassell <bcassell@umich.edu>
2012-08-21 14:12:58 -0400

    lower load poker

--- flux_poker.rb
+++ flux_poker.rb
@@ -5,8 +5,7 @@ flux_proxy = DRbObject.new_with_uri('druby://localhost:30000')
 loop do
   if flux_proxy.closed?
   else
-    out = flux_proxy.exec!("qstat -n1 | grep egta-")
-    puts "#{ out ? out.split("\n").count : 0 } current job(s)"
+    puts flux_proxy.exec!("date")
   end
   sleep 59
 end
